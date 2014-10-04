c7c2695e4e0aa550f7fe56271d14051a95561f5c
Jose Borreguero <borreguero@gmail.com>
2014-10-04 14:20:36 -0400

    Added notebook opener

---
+++ ipython_notebook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Instantiate a python notebook')
+parser.add_argument('dir', help='directory containing the notebook.')
+args=parser.parse_args()
+
+if not os.path.exists(args.dir):
+    raise IOError , 'project {0} not found'.format(args.dir)
+    sys.exit(1)
+
+os.system( ' ipython notebook --pylab inline --notebook-dir={0}'.format(args.dir) )
