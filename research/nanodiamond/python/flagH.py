54be5d908b3ae209f72f3e2f02218dd4bf3257c7
Jose Borreguero <borreguero@gmail.com>
2015-03-25 16:35:51 -0400

    Updated nanodiamond

--- research/nanodiamond/python/flagH.py
+++ research/nanodiamond/python/flagH.py
@@ -7,29 +7,32 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Create PDB file tagging selected hydrogens.')
 parser.add_argument('infile', help='input PDB file')
-parser.add_argument('selection', type=str, help='One of: RNA, water')
+parser.add_argument('selection', type=str, help='One of: RNA, water, diamond')
 parser.add_argument('outfile', help='output PDB file')
 args=parser.parse_args()
 
-# Check input first
-if not os.path.exists(args.infile):
-    raise IOError(args.infile)
-
-sels=['RNA', 'water']
+sels=['RNA', 'water', 'diamond']
 if args.selection not in sels:
     raise IOError('selection must be one of {0}'.format(','.join(sels)) )
-
-if not os.path.dirname(args.outfile):
-    raise IOError('directory {0} does not exists'.format(os.path.dirname(args.outfile)))
 
 buf=''
 for line in open(args.infile).readlines():
     bufline = line
+    if line[0:5]=='ATOM ':
+        if (line[13]=='H' or line[12]=='H'):
+            resname = line[17:21]
+            if (args.selection=='water' and resname=='TIP3') or \
+               (args.selection=='RNA' and resname in ['ADE ', 'CYT ', 'GUA ', 'URA ']):
+                bufline = line[:62] + '1.00' + line[66:] # Flag by setting B-factor as 1.00
+        elif (args.selection=='diamond' and line[17:22]=='NDB1N'):
+            bufline = line[:62] + '1.00' + line[66:] # Flag by setting B-factor as 1.00
+            
     if line[0:5]=='ATOM ' and (line[13]=='H' or line[12]=='H'):
         resname = line[17:21]
         if (args.selection=='water' and resname=='TIP3') or \
            (args.selection=='RNA' and resname in ['ADE ', 'CYT ', 'GUA ', 'URA ']):
             bufline = line[:62] + '1.00' + line[66:] # Flag by setting B-factor as 1.00
+
     buf += bufline
 
 open(args.outfile,'w').write(buf)
