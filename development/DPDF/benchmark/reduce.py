72e05272ed9b72a3b20f93af85e6f1961f0394c9
Jose Borreguero <borreguero@gmail.com>
2015-06-18 18:58:15 -0400

    Updated DPDF

--- development/DPDF/benchmark/reduce.py
+++ development/DPDF/benchmark/reduce.py
@@ -1,6 +1,6 @@
 #Script input parameters
 wd='/projects/development/DPDF/benchmark'
-dE=1.0 #1meV binning in energy
+dE=1.5 #1meV binning in energy
 dtheta=0.15 #binning in theta angle. Advised is 0.2degrees
 Qmin=0.0
 Qmax=15.0
@@ -21,7 +21,7 @@ Load(Filename='+'.join(event_files),OutputWorkspace='events_sum')
 #Load the vanadium file, assume to be preprocessed, meaning that for every detector
 #all events whithin a particular wide wavelength range have been rebinned into a
 #single histogram
-Load(Filename='van56293.nxs', OutputWorkspace='van')
+Load(Filename=wd+'/'+'van56293.nxs', OutputWorkspace='van')
 
 #Retrieve the mask from the vanadium workspace, and apply it to the data
 MaskDetectors(Workspace='events_sum', MaskedWorkspace='van')
@@ -36,6 +36,9 @@ Ei_max=0.95*Ei
 Erange=','.join([str(x) for x in [Ei_min,dE,Ei_max]])
 DgsReduction(SampleInputWorkspace='events_sum', EnergyTransferRange=Erange, OutputWorkspace='reduced')
 
+#Clean up the events files. They take a lot of space in memory
+DeleteWorkspace('events_sum')
+
 #Convert to S(theta,E)
 GenerateGroupingPowder(InputWorkspace='reduced', AngleStep=dtheta, GroupingFilename=wd+'/detectors_to_angle.xml')
 GroupDetectors(InputWorkspace='reduced', MapFile=wd+'/detectors_to_angle.xml', OutputWorkspace='ste')
@@ -44,15 +47,25 @@ GroupDetectors(InputWorkspace='reduced', MapFile=wd+'/detectors_to_angle.xml', O
 #every detector has all energies into a single bin, so we get S(theta) instead of S(theta,E)
 GroupDetectors(InputWorkspace='van', MapFile=wd+'/detectors_to_angle.xml', OutputWorkspace='st_van')
 Divide('ste','st_van',OutputWorkspace='ste_normalized')
+ClearMaskFlag(Workspace='ste_normalized')
 
+#Linear interpolation
+#Assumptions:
+# 1. There are energy transfer with no signal whatsoever, S(theta, E*)=0.0 for all theta
+# 2. Null spectra at E* are flanked with non-null spectra at E*-dE and E*+dE
+CloneWorkspace(InputWorkspace='ste_normalized', OutputWorkspace='ste_normalized_interp')
+w=mtd['ste_normalized_interp']
+for iHist in range(1,w.getNumberHistograms()-1):
+    if not w.dataY(iHist).any():
+        w.dataY(iHist)[:] = 0.5*(w.dataY(iHist-1)[:] + w.dataY(iHist+1)[:])
 #Mask the problematic angles
 #MaskAngle()
 
-#Convert S(\theta,E) to S(Q,E), then rebin to 
-ConvertToMD(InputWorkspace='ste_normalized', QDimensions='|Q|', dEAnalysisMode='Direct', OutputWorkspace='sqe')
-Qrange='|Q|,{0},{1}.{2}'.format(Qmin,Qmax,int( (Qmax-Qmin)/dQ ))
-deltaErange='DeltaE,{0},{1}.{2}'.format(Ei_min, Ei_max, int( (Ei_max-Ei_min)/dE ))
-BinMD(InputWorkspace='sqe', AxisAligned=1, AlignedDim0='|Q|,0,15,75',
+#Convert S(theta,E) to S(Q,E), then rebin in |Q| and E to MD workspace 
+ConvertToMD(InputWorkspace='ste_normalized_interp', QDimensions='|Q|', dEAnalysisMode='Direct', OutputWorkspace='sqe')
+Qrange='|Q|,{0},{1},{2}'.format(Qmin,Qmax,int( (Qmax-Qmin)/dQ ))
+deltaErange='DeltaE,{0},{1},{2}'.format(Ei_min, Ei_max, int( (Ei_max-Ei_min)/dE ))
+BinMD(InputWorkspace='sqe', AxisAligned=1, AlignedDim0=Qrange,
     AlignedDim1=deltaErange, OutputWorkspace='sqe_binned')
 
 #Slice the data by transforming to a Matrix2Dworkspace, with deltaE along the vertical axis
