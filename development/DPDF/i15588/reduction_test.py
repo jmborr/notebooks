d60a472777c4fb0d42565e990e2ee9af8b16614f
Jose Borreguero <borreguero@gmail.com>
2016-03-07 20:32:21 -0500

    Added changes to basis reduction and DPDF

---
+++ development/DPDF/i15588/reduction_test.py
@@ -0,0 +1,7 @@
+DPDFreduction(RunNumbers="56415-56420",
+    Vanadium="/projects/development/DPDF/benchmark/van56293.nxs",
+    EmptyCanRunNumbers="34665+34666+34667+34668+34669+34670+34671+34672+34673+34674+34685+34686+34687+34688+34689+34690+34691+34692",
+    EnergyBins="1.5",
+    MomentumTransferBins="0.1",
+    CleanWorkspaces=1,
+    OutputWorkspace="slices_QE")  
